792eb0263236e1810014c84387b07f6b6c8567f6
kapildev_in <kapil060398@gmail.com>
2025-05-08 12:47:32 +0530

    Amazon Clone HTML converted in Wordpress

---
+++ header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1: Amazon Clone</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+    <header>
+
+        <div class="navbar ">
+
+            <div class="nav-logo border">
+                <div class="logo"></div>
+            </div>
+            
+
+            <div class="nav-address border">
+                <p class="add-first"> Deliver to</p>
+                <div class="add-icon">
+                    <i class="fa-solid fa-location-dot add-icon-class"></i>
+                    <p class="add-second">India</p>
+                </div>
+            </div>
+
+
+            <div class="nav-search">   
+                <select class="search-select">
+                    <option>All</option>
+                    <option>Electronics</option>
+                </select>
+                <input placeholder="Search Amazon" class="search-box">
+                <div class="search-icon">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+
+            <div class="nav-account border">  
+            <p class="nav-account-p1">Hello, sign in</p>
+            <p class="nav-account-p2">Account & Lists</p>
+
+            </div>
+
+            <div class="nav-orders border">
+                <p class="nav-orders-p1">Returns</p>
+                <p class="nav-orders-p2">& Orders</p>
+                </div>
+
+             <div class="nav-cart border">
+                <i class="fa-solid fa-cart-shopping cart-icon" style="color: #ffffff;"></i>
+                <p>Cart</p>
+             </div> 
+             
+         </div>    
+
+        <div class="header-panel">
+            <div class="panel-menu border">
+                <i class="fa-solid fa-bars"></i>
+                <p>All</p>
+                
+            </div>
+
+            <div class="panel-ops">
+                <p class="border">Today's Deals</p>
+                <p class="border">Customer Service</p>
+                <p class="border">Registry</p>
+                <p class="border">Gift Cards</p>
+                <p class="border">Sell</p>
+                
+            </div>
+
+            <div class="panel-ops-right">
+                <p class="border">Track Your Orders</p>
+                
+            </div>
+
+
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,178 @@
+  <?php  get_header(); ?>
+  
+  <main>
+  <div class="hero-section">   
+        <img src="<?php echo get_template_directory_uri();?>/images/Amazon_banner.jpg" alt="slider" class="banner">
+
+        <p class="slider-patch">You are on amazon.com. You can also shop on Amazon India for millions of products with fast local delivery. <a href="https://www.amazon.in" target="_main"> Click here to go to amazon.in </a> </p>
+        
+        <div class="hero-box-container">
+
+            <div class="hero-box">
+                <div class="hero-box-inner1">
+                    <h3> Gaming accessories</h3>
+                </div>
+                <div class="hero-box-grid">
+                    <div class="hero-box-item">
+                        <img src="<?php echo get_template_directory_uri();?>/images/Fuji_Quad_Headset_box1.jpg">
+                        <p>Headset</p>
+                     </div>
+                        
+                     <div class="hero-box-item">
+                        <img src="<?php echo get_template_directory_uri();?>/images/Fuji_Quad_Chair_box1.jpg">
+                        <p>Chair</p>
+                </div> 
+                    
+                    <div class="hero-box-item"> 
+                        <img src="<?php echo get_template_directory_uri();?>/images/Fuji_Quad_Keyboard_box1.jpg">
+                        <p>keyboard</p>
+                    </div>
+                        
+                    <div class="hero-box-item"> 
+                        <img src="<?php echo get_template_directory_uri();?>/images/Fuji_Quad_Mouse_box1.jpg">
+                        <p>Mouse</p>
+                    </div>
+                        
+                              
+                </div>
+                <a href="#" class="see-more">See more</a>
+            </div>
+
+            
+
+            <div class="hero-box">
+                <div class="hero-box-inner1">
+                    <h3> Get your game on</h3>
+                </div>
+
+                <div class="hero-box-big-image"> 
+                    <img src="<?php echo get_template_directory_uri();?>/images/cpu.jpg">
+                </div>
+                <a href="#" class="see-more"> Discover more in Home </a>
+ 
+            </div>
+
+            <div class="hero-box">
+                <div class="hero-box-inner1">
+                    <h3> New year, new supplies</h3>
+                </div>
+
+                <div class="hero-box-big-image"> 
+                    <img src="<?php echo get_template_directory_uri();?>/images/stationary.jpg">
+                </div>
+                <a href="#" class="see-more"> Shop office products </a>
+ 
+            </div>
+
+
+            <div class="hero-box">
+                <div class="hero-box-inner1">
+                    <h3> Gaming Corner</h3>
+                </div>
+
+                <div class="hero-box-big-image"> 
+                    <img src="<?php echo get_template_directory_uri();?>/images/gaming_zone.jpg">
+                </div>
+                <a href="#" class="see-more"> Find all your gaming needs </a>
+ 
+            </div>
+
+        </div>
+
+    </div>
+
+    
+
+        <div class="body-box body-box-top">
+            <div class="box1 box">
+            <div class="body-box-heading">
+                <h3>Online Accessories</h3>
+            </div>
+            <div class="hero-box-big-image">
+                <img src="<?php echo get_template_directory_uri();?>/images/accessories.jpg">
+            </div>
+            <a href="#" class="see-more">Shop accessories</a>
+        </div>
+
+        <div class="box1 box">
+            <div class="body-box-heading">
+                <h3>Online Buds</h3>
+            </div>
+            <div class="hero-box-big-image">
+                <img src="<?php echo get_template_directory_uri();?>/images/noise.jpg">
+            </div>
+            <a href="#" class="see-more">Shop earbuds now</a>
+        </div>
+        
+        <div class="box1 box">
+            <div class="body-box-heading">
+                <h3>Hoodies on sale</h3>
+            </div>
+            <div class="hero-box-big-image">
+                <img src="<?php echo get_template_directory_uri();?>/images/hoodies.jpg">
+            </div>
+            <a href="#" class="see-more">Shop now</a>
+        </div>
+
+        <div class="box1 box">
+            <div class="body-box-heading">
+                <h3>20% off on home essentials</h3>
+            </div>
+            <div class="hero-box-big-image">
+                <img src="<?php echo get_template_directory_uri();?>/images/vacuum_cleaner.webp">
+            </div>
+            <a href="#" class="see-more">Shop vaccume cleaner</a>
+        </div>
+
+    </div>
+
+    <div class="body-box">
+
+        <div class="box1 box">
+            <div class="body-box-heading">
+                <h3>Fill your basket with joy</h3>
+            </div>
+            <div class="hero-box-big-image">
+                <img src="<?php echo get_template_directory_uri();?>/images/Easter.jpg">
+            </div>
+            <a href="#" class="see-more">Shop for Easter</a>
+        </div>
+
+        <div class="box1 box">
+            <div class="body-box-heading">
+                <h3>Limited time deals</h3>
+            </div>
+            <div class="hero-box-big-image">
+                <img src="<?php echo get_template_directory_uri();?>/images/voltas.jpg">
+            </div>
+            <a href="#" class="see-more">50% off on best seller</a>
+        </div>
+
+        <div class="box1 box">
+            <div class="body-box-heading">
+                <h3>Up to 50% off on CPU</h3>
+            </div>
+            <div class="hero-box-big-image">
+                <img src="<?php echo get_template_directory_uri();?>/images/cpu.jpg">
+            </div>
+            <a href="#" class="see-more">See all offers</a>
+        </div>
+
+        <div class="box1 box">
+            <div class="body-box-heading">
+                <h3>Best seller in mobiles</h3>
+            </div>
+            <div class="hero-box-big-image">
+                <img src="<?php echo get_template_directory_uri();?>/images/mobile.jpg">
+            </div>
+            <a href="#" class="see-more">Explore Now</a>
+        </div>
+
+
+    </div>
+
+        </main>
+        
+<?php get_footer(); ?>
+
+    
