329481cac9bc88ac4e28bb0e35779e29d0ddd436
kapildev_in <kapil060398@gmail.com>
2025-05-13 16:09:25 +0530

    page.php page layout design, added some style on page title and page body

--- page.php
+++ page.php
@@ -3,6 +3,26 @@
 ?>
 
 
+<main class="page-content">
+    <section class="content-wrapper">
+    <div class="container">
+    <h1 class="page-title">
+    <?php the_title(); ?>
+    </h1>
+
+    <div class="page-body">
+        <?php the_content(); ?>
+    </div>
+    </div>
+
+    </section>
+
+</main>
+
+
+
+
+
 
 <?php 
     get_footer();
