169b9d749d1c7f5fee3486c6edeab30f72e580ad
kapildev_in <kapil060398@gmail.com>
2025-05-13 18:30:16 +0530

    added set feature image option iside admin panel of page and post panel

--- functions.php
+++ functions.php
@@ -4,5 +4,6 @@ register_nav_menus (
     array ('primary-menu'=>'Top Menu')
 );
 
+add_theme_support ('post-thumbnails');
 
 ?>
--- page.php
+++ page.php
@@ -6,21 +6,17 @@
 <main class="page-content">
 
     <div class="breadcrumb">
-    <a href="<?php echo home_url(); ?>">Home</a>/<strong><?php the_title(); ?> </strong>
-
+        <a href="<?php echo home_url(); ?>">Home</a>/<strong><?php the_title(); ?> </strong>
     </div>
 
     <section class="content-wrapper">
-    <div class="container">
-    <h1 class="page-title">
-    <?php the_title(); ?>
-    </h1>
+        <div class="container">
+            <h1 class="page-title"> <?php the_title(); ?> </h1>
 
-    <div class="page-body">
-        <?php the_content(); ?>
-    </div>
-    </div>
-
+            <div class="page-body">
+                <?php the_content(); ?>
+            </div>
+        </div>
     </section>
 
 </main>
