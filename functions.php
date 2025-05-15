6230ac93afb5ebda79030872afdf09a6001cf739
kapildev_in <kapil060398@gmail.com>
2025-05-15 12:43:50 +0530

    Comment added inside functions

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php 
 
 register_nav_menus (
-    array ('primary-menu'=>'Top Menu')
+    array ('primary-menu'=>'Top Menu')  //used to add menu option inside admin dashboard
 );
 
-add_theme_support ('post-thumbnails');
+add_theme_support ('post-thumbnails'); //used to add feature image option in admin panel
 
 ?>
