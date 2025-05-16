ae00d1a25c70a4244f5eb1c3bcf452d3bbe9ebe6
kapildev_in <kapil060398@gmail.com>
2025-05-16 12:44:47 +0530

    Page breadcrumb added, minor style added

--- template-contact.php
+++ template-contact.php
@@ -1,5 +1,23 @@
 <?php
 //Template Name:Contact Us
 
+get_header();
 ?>
 
+<main class="page-contact">
+
+    <div class="breadcrumb">
+        <a href="<?php home_url(); ?>">Home</a>/<strong> <?php the_title(); ?> </strong>
+    </div>
+
+    <section class="">
+
+    </section>
+
+</main>
+
+<?php 
+
+get_footer();
+
+?>
